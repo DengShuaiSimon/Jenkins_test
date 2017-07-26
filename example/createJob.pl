5573137c8bdb5c0c089067c423c364e3669192fb
DengShuaiSimon <denfshuaishuai@icloud.com>
2017-07-26 17:53:22 +0800

    Update createJob.pl

--- example/createJob.pl
+++ example/createJob.pl
@@ -23,7 +23,7 @@ unless($api->check_jenkins_url)
 
 
 
-##get config.xml template
+###get config.xml template
 #my $result = system("curl -X GET http://dengshuai_super:8080\@127.0.0.1:8080/job/JobTest/config.xml >./config.xml");
 #print "result: $result\n";
 
@@ -39,22 +39,45 @@ my $read_line;
      close(FILE);
 
 	 
-##project_config() returns the configuration for the project in xml. To get the template of the config.xml from other project.
-my $project_name = "JobTest";
-my $config = $api->project_config($project_name);
-print "config : $config";
+###project_config() returns the configuration for the project in xml. To get the template of the config.xml from other project.
+#my $project_name = "JobTest";
+#my $config = $api->project_config($project_name);
+#print "config : $config\n";
 
 
 ###
-my $name = "test7";
+my $job_name = "test7";
 #my $job = $api->create_job($name,$config_string);
-my $setconfigresult = $api->set_project_config($name,$config_string);
+my $setconfigresult = $api->set_project_config($job_name,$config_string);
 
 
+###Trigger a build
+my $success = $api->trigger_build($job_name);
+    unless($success)
+    {
+        if($api->response_code == 403)
+        {
+            print "Auth failure\n";
+        }
+        else
+        {
+            print $api->response_content;
+        }
+    }
 
-
-
-
+my $jobs = $api->current_status({ extra_params => { tree => 'jobs[name,color]' } });
+my @job_list = @{$jobs->{jobs}};
+#@job_list = sort { $a->{name} cmp $b->{name} } @job_list;
+for my $job (@job_list)
+{
+    # status isn't really this simple, but unstable doesn't
+    # map very to the todos in perl, so I'm not interested
+    # in it personally.
+	if($job->{name} eq $job_name){
+    my $status = $job->{color} eq 'blue' ? 'OK' : 'Fail';
+    print "$job->{name} - $status\n";
+	}#if
+}
 
 
 
